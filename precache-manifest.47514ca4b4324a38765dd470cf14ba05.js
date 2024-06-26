4a1eda748684fc8db5a2dee5afa88db125792945
Ishita <ishitasaini567@gmail.com>
2024-06-26 20:07:38 +0530

    Updates

---
+++ precache-manifest.47514ca4b4324a38765dd470cf14ba05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e75a4bb9a1b4643827a7c16c33a886",
+    "url": "/text-utils/index.html"
+  },
+  {
+    "revision": "f53633cbd29b4b8277c6",
+    "url": "/text-utils/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9f6b2f06818f226aadfc",
+    "url": "/text-utils/static/js/2.9b1344a3.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/text-utils/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "f53633cbd29b4b8277c6",
+    "url": "/text-utils/static/js/main.296700a5.chunk.js"
+  },
+  {
+    "revision": "c602083bde2d254ee96f",
+    "url": "/text-utils/static/js/runtime~main.6df794fe.js"
+  }
+]);
